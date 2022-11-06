9504821a2225e5b8aa23cb8f6ea0023ac178fc06
Yuri Norwood <106889957+norwd@users.noreply.github.com>
2022-11-07 11:15:34 +1300

    Quote bin path when `cd`ing into it to avoid shell globbing
    Signed-off-by: Yuri Norwood <106889957+norwd@users.noreply.github.com>

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function downloadRelease(octokit, os, org, repo, release, token) {
 
             // Extract the archive into the install target
             console.log("Unpacking archive file...")
-            execSync(`cd ${binpath} && ${extract} ${archive}`)
+            execSync(`cd "${binpath}" && ${extract} ${archive}`)
 
             // Remove the downloaded asset archive
             console.log("Removing asset archive...")
