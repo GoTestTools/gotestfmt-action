12174122f2826bb8b25c8b90f9a6e1f7f829208d
Yuri Norwood <106889957+norwd@users.noreply.github.com>
2022-11-07 11:01:48 +1300

    Remove redundant pathsep variable
    Signed-off-by: Yuri Norwood <106889957+norwd@users.noreply.github.com>

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ async function downloadRelease(octokit, os, org, repo, release, token) {
     })
 
     const postfix = `_${os}_amd64.${os === "windows" ? "zip" : "tar.gz"}`;
-    const pathsep = os === "windows" ? "\\" : "/";
     const tempdir = os === "windows" ? process.env.TEMP : "/tmp";
-    const binpath = os === "windows" ? "C:\\Program Files\\GoTestTools" : "/usr/local/bin";
+    const binpath = os === "windows" ? "C:\\Program Files\\GoTestTools\\" : "/usr/local/bin/";
     const extract = os === "windows" ? "tar -xvf" : "tar -xvzf";
-    const archive = `${tempdir}${pathsep}gotestfmt${postfix}`;
+    const archive = `${tempdir}gotestfmt${postfix}`;
 
     if (os === "windows") {
         execSync(`"${binpath}" >> $env:GITHUB_PATH`);
